4b9bcedefca3f09a6ff7108b610bdb10aad0bb89
NGUYEN NGOC TUAN <VACNGTU001@VIETACORP.NET>
2020-06-01 16:44:43 +0700

    init source

---
+++ resources/views/back/layout/footer.blade.php
@@ -0,0 +1,15 @@
+
+		<!--begin::Global Theme Bundle(used by all pages)-->
+		<script src="back/assets/plugins/global/plugins.bundle.js"></script>
+		<script src="back/assets/plugins/custom/prismjs/prismjs.bundle.js"></script>
+		<script src="back/assets/js/scripts.bundle.js"></script>
+		<!--end::Global Theme Bundle-->
+		<!--begin::Page Vendors(used by this page)-->
+		<script src="back/assets/plugins/custom/fullcalendar/fullcalendar.bundle.js"></script>
+		<!--end::Page Vendors-->
+		<!--begin::Page Scripts(used by this page)-->
+		<script src="back/assets/js/pages/widgets.js"></script>
+		<!--end::Page Scripts-->
+	</body>
+	<!--end::Body-->
+</html>
