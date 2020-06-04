cc7c5501c79af8b9b5730c96ae4f18f006f8e58e
John Nguyen <tuannguyen0719@gmail.com>
2020-06-04 13:41:00 +0700

    add data sider bar

---
+++ database/migrations/2020_06_04_021033_create_category.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('keyword');
+            $table->longText('description');
+            $table->increments('parent_id');
+            $table->string('meta_title');
+            $table->string('meta_keyword');
+            $table->longText('meta_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category');
+    }
+}
---
+++ database/migrations/2020_06_04_021757_create_blog.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('keyword');
+            $table->mediumText('short_description');
+            $table->longText('description');
+            $table->increments('category_id');
+            $table->string('meta_title');
+            $table->string('meta_keyword');
+            $table->longText('meta_description');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog');
+    }
+}
