b3051962a3541d4a81e1f8cc08e8827c38f1d368
John Nguyen <tuannguyen0719@gmail.com>
2020-06-05 14:13:02 +0700

    Deploy

--- resources/views/back/layout/header.blade.php
+++ resources/views/back/layout/header.blade.php
@@ -13,14 +13,14 @@
         <!--end::Page Vendors Styles-->
         <!--begin::Global Theme Styles(used by all pages)-->
         <link href="{{ asset('back/assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{ asset('back/assets/plugins/custom/prismjs/prismjs.bundle.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{ asset('back/assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('back/assets/plugins/custom/prismjs/prismjs.bundle.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('back/assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
         <!--end::Global Theme Styles-->
         <!--begin::Layout Themes(used by all pages)-->
-        <link href="{ asset('back/assets/css/themes/layout/header/base/light.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{ asset('back/assets/css/themes/layout/header/menu/light.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{ asset('back/assets/css/themes/layout/brand/dark.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{ asset('back/assets/css/themes/layout/aside/dark.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('back/assets/css/themes/layout/header/base/light.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('back/assets/css/themes/layout/header/menu/light.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('back/assets/css/themes/layout/brand/dark.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('back/assets/css/themes/layout/aside/dark.css') }}" rel="stylesheet" type="text/css" />
         <!--end::Layout Themes-->
         <link rel="shortcut icon" href="{{ asset('back/assets/media/logos/favicon.ico') }}" />
     </head>
