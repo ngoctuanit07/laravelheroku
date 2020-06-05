0c2055c1df5791b35374373a57a0a0387b908c0d
John Nguyen <tuannguyen0719@gmail.com>
2020-06-05 14:03:25 +0700

    Deploy

--- resources/views/back/layout/footer.blade.php
+++ resources/views/back/layout/footer.blade.php
@@ -1,14 +1,14 @@
 
 		<!--begin::Global Theme Bundle(used by all pages)-->
-		<script src="back/assets/plugins/global/plugins.bundle.js"></script>
-		<script src="back/assets/plugins/custom/prismjs/prismjs.bundle.js"></script>
-		<script src="back/assets/js/scripts.bundle.js"></script>
+		<script src="{{ asset('back/assets/plugins/global/plugins.bundle.js') }}"></script>
+		<script src="{{ asset('back/assets/plugins/custom/prismjs/prismjs.bundle.js') }}"></script>
+		<script src="{{ asset('back/assets/js/scripts.bundle.js') }}"></script>
 		<!--end::Global Theme Bundle-->
 		<!--begin::Page Vendors(used by this page)-->
-		<script src="back/assets/plugins/custom/fullcalendar/fullcalendar.bundle.js"></script>
+		<script src="{{ asset('back/assets/plugins/custom/fullcalendar/fullcalendar.bundle.js') }}"></script>
 		<!--end::Page Vendors-->
 		<!--begin::Page Scripts(used by this page)-->
-		<script src="back/assets/js/pages/widgets.js"></script>
+		<script src="{{ asset('back/assets/js/pages/widgets.js') }}"></script>
 		<!--end::Page Scripts-->
 	</body>
 	<!--end::Body-->
--- resources/views/back/layout/master.blade.php
+++ resources/views/back/layout/master.blade.php
@@ -6,7 +6,7 @@
 		<div id="kt_header_mobile" class="header-mobile align-items-center header-mobile-fixed">
 			<!--begin::Logo-->
 			<a href="index.html">
-				<img alt="Logo" src="back/assets/media/logos/logo-light.png" />
+				<img alt="Logo" src="asset('back/assets/media/logos/logo-light.png')" />
 			</a>
 			<!--end::Logo-->
 			<!--begin::Toolbar-->
