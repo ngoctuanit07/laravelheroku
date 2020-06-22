925cfa14343b5fac8382965dc7c3789fbcf92393
John Nguyen <tuannguyen0719@gmail.com>
2020-06-22 10:26:55 +0700

    add database contact

---
+++ database/migrations/2020_06_18_121043_create_contact.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContact extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->bigInteger('id');
+            $table->char('name', 100);
+            $table->char('email', 100);
+            $table->integer('phonenumber', 100);
+            $table->text('title');
+            $table->longText('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact');
+    }
+}
